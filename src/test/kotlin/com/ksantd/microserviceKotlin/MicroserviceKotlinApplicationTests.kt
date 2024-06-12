d4e912d8da8208f296591a91e046f8642906a3c0
ksantd <admin@ksantd.com>
2024-06-13 00:55:43 +0300

    add template

---
+++ src/test/kotlin/com/ksantd/microserviceKotlin/MicroserviceKotlinApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.ksantd.microserviceKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+
+@Import(TestcontainersConfiguration::class)
+@SpringBootTest
+class MicroserviceKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
